72628bdd9b9049cb39ab0d6cd0e8898f7de09ba7
Eddy Guo <eddyyanruguo@gmail.com>
2021-02-21 11:56:25 -0500

    fix vscode false warning, remove coded solution

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    objects = models.Manager()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
