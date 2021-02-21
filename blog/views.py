25824cf0dc815fb35973467dda5bd5668d506bb9
Eddy Guo <eddyyanruguo@gmail.com>
2021-02-21 01:04:10 -0500

    remove dummy data, database/migration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Eddy Guo',
-        'title': 'Blog Post 1',
-        'content': 'First Post', 
-        'date_posted': 'February 20, 2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post', 
-        'date_posted': 'February 20, 2021'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
