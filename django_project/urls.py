bc82c95db3d2a6244432c3bc5d759b476a965470
Eddy Guo <eddyyanruguo@gmail.com>
2021-03-06 15:11:52 -0500

    reupload all files

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,7 @@
-"""django_blog URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -29,6 +29,6 @@ urlpatterns = [
     path('', include('blog.urls')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -11,7 +10,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, **kwargs):
+    def save(self):
         super().save()
 
         img = Image.open(self.image.path)
